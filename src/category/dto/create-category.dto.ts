3746f9c2ab34a63b8ea63cdf48f56b0946c3b17a
Flame <5017_30@mail.ru>
2023-12-27 01:26:00 +0300

    add: CRUD categories

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,10 @@
-export class CreateCategoryDto {}
+import { IsNotEmpty, IsOptional } from 'class-validator'
+import { UserEntity } from '../../user/entities/user.entity'
+
+export class CreateCategoryDto {
+  @IsNotEmpty()
+  title: string
+
+  @IsOptional()
+  user?: UserEntity
+}

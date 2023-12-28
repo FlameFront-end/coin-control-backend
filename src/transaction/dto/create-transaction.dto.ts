b06d79b7de5bbac3b6a9744ccaef45c89cc74f58
Flame <5017_30@mail.ru>
2023-12-29 00:18:07 +0300

    add: transactions CRUD & pagination

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTransactionDto {}
+import { CategoryEntity } from '../../category/entities/category.entity'
+import { UserEntity } from '../../user/entities/user.entity'
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
+
+export class CreateTransactionDto {
+  @IsNotEmpty()
+  title: string
+
+  @IsNotEmpty()
+  @IsNumber()
+  amount: number
+
+  @IsString()
+  type: 'expense' | 'income'
+
+  category: CategoryEntity
+
+  user: UserEntity
+}
--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,34 +1,73 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TransactionService } from './transaction.service';
-import { CreateTransactionDto } from './dto/create-transaction.dto';
-import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UsePipes,
+  ValidationPipe,
+  UseGuards,
+  Req,
+  Query
+} from '@nestjs/common'
+import { TransactionService } from './transaction.service'
+import { CreateTransactionDto } from './dto/create-transaction.dto'
+import { UpdateTransactionDto } from './dto/update-transaction.dto'
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
 
-@Controller('transaction')
+@Controller('transactions')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
+  // Должен быть первым!!!
+  // transactions/pagination?page=1&limit=3
+  @Get('pagination')
+  @UseGuards(JwtAuthGuard)
+  findAllWithPagination(
+    @Req() req,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 5
+  ) {
+    return this.transactionService.findAllWithPagination(
+      +req.user.id,
+      +page,
+      +limit
+    )
+  }
+
   @Post()
-  create(@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionService.create(createTransactionDto);
+  @UsePipes(new ValidationPipe())
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createTransactionDto: CreateTransactionDto, @Req() req) {
+    return this.transactionService.create(createTransactionDto, +req.user.id)
   }
 
   @Get()
-  findAll() {
-    return this.transactionService.findAll();
+  @UseGuards(JwtAuthGuard)
+  findAll(@Req() req) {
+    return this.transactionService.findAll(Number(req.user.id))
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
-    return this.transactionService.findOne(+id);
+    return this.transactionService.findOne(+id)
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
-    return this.transactionService.update(+id, updateTransactionDto);
+  @UseGuards(JwtAuthGuard)
+  update(
+    @Param('id') id: string,
+    @Body() updateTransactionDto: UpdateTransactionDto
+  ) {
+    return this.transactionService.update(+id, updateTransactionDto)
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
-    return this.transactionService.remove(+id);
+    return this.transactionService.remove(+id)
   }
 }

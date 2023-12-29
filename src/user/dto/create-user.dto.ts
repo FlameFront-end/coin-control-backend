ef158346ac220f566b0fa140fbee628084938a4c
Flame <5017_30@mail.ru>
2023-12-29 15:50:44 +0300

    refactor

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,7 @@
 import { IsEmail, MinLength } from 'class-validator'
 
 export class CreateUserDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'Invalid email' })
   email: string
 
   @MinLength(6, { message: 'Password must ne more then 6 symbols' })
